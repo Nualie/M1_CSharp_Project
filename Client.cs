2b40a7ba15466269f084db4c3518f30fb976299e
Nualie <56164296+Nualie@users.noreply.github.com>
2021-10-21 00:52:29 +0200

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Client
+{
+	public Client()
+	{
+	}
+}
+
+/*
+ * Client:
+ • A GUID (use guid library in c#), GUID is a global unique id
+• A first name and last name, not unique
+• A pin, unique with the same guid
+• List of currencies and amount in each currency (use list)
+• Main currency
+• Any other important fields for the smooth run of the software
+*/
